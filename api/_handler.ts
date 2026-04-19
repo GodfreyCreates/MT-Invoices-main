e96d6098c36b844c31e006baa6c5e5180f98abba
productioncodebase <productioncodebase@gmail.com>
2026-04-19 18:59:28 +0200

    ddo

--- api/_handler.ts
+++ api/_handler.ts
@@ -11,7 +11,26 @@ async function getApp() {
   return appPromise;
 }
 
+function restoreApiPathFromRewrite(req: IncomingMessage) {
+  const currentUrl = req.url ?? "/api";
+  const parsedUrl = new URL(currentUrl, "http://vercel.local");
+  const rewrittenPath = parsedUrl.searchParams.get("__pathname");
+
+  if (!rewrittenPath) {
+    return;
+  }
+
+  parsedUrl.searchParams.delete("__pathname");
+
+  const normalizedPath = rewrittenPath.replace(/^\/+/, "");
+  parsedUrl.pathname = normalizedPath ? `/api/${normalizedPath}` : "/api";
+
+  const search = parsedUrl.searchParams.toString();
+  req.url = `${parsedUrl.pathname}${search ? `?${search}` : ""}`;
+}
+
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  restoreApiPathFromRewrite(req);
   const app = await getApp();
   return app(req as never, res as never);
 }
